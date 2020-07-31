ec2efee397a56e4e35b1b69750a37761adc548fa
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-07-31 19:24:35 +0100

    add modal login markup

--- components/ModalLogin.jsx
+++ components/ModalLogin.jsx
@@ -1,15 +1,40 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { TouchableOpacity } from "react-native";
 
 const ModalLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginHandler = () => {
+      
+  };
+
   return (
     <Container>
       <Modal>
-        <Logo />
-        <Text></Text>
-        <TextInput />
-        <TextInput />
-        <Button />
+        <Logo source={require("../assets/logo-dc.png")} />
+        <Text>Start Learning</Text>
+
+        <TextInput
+          placeholder="Email"
+          keyboardType="email-address"
+          onChangeText={(text) => setEmail(text)}
+        />
+
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <IconEmail source={require("../assets/icon-email.png")} />
+        <IconPassword source={require("../assets/icon-password.png")} />
+
+        <TouchableOpacity onPress={loginHandler}>
+          <Button>
+            <ButtonText>Login</ButtonText>
+          </Button>
+        </TouchableOpacity>
       </Modal>
     </Container>
   );
@@ -34,12 +59,66 @@ const Modal = styled.View`
   background: white;
   border-radius: 20px;
   box-shadow: 0 20px 40px rgba(0, 0, 0, 0.15);
+  align-items: center;
 `;
 
-const Logo = styled.Image``;
+const Logo = styled.Image`
+  width: 44px;
+  height: 44px;
+  margin-top: 50px;
+`;
 
-const Text = styled.Text``;
+const Text = styled.Text`
+  margin-top: 20px;
+  font-size: 13px;
+  font-weight: 600;
+  text-transform: uppercase;
+  width: 160px;
+  text-align: center;
+  color: #b8bece;
+`;
 
-const TextInput = styled.TextInput``;
+const TextInput = styled.TextInput`
+  border: 1px solid #dbdfea;
+  width: 295px;
+  height: 44px;
+  border-radius: 10px;
+  font-size: 17px;
+  color: #3c4560;
+  margin-top: 20px;
+  padding-left: 44px;
+`;
 
-const Button = styled.View``;
+const Button = styled.View`
+  background: #5263ff;
+  width: 295px;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  box-shadow: 0 10px 20px #c2cbff;
+  margin-top: 20px;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+  font-weight: 600;
+  font-size: 20px;
+  text-transform: uppercase;
+`;
+
+const IconEmail = styled.Image`
+  width: 24px;
+  height: 16px;
+  position: absolute;
+  top: 165px;
+  left: 31px;
+`;
+
+const IconPassword = styled.Image`
+  width: 18px;
+  height: 24px;
+  position: absolute;
+  top: 225px;
+  left: 35px;
+`;
